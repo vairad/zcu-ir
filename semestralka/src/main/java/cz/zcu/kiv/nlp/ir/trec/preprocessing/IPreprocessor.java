2e75dca18ae8a52a48482dce372b5c4589105bf6
Radek Vais <vais.radek@gmail.com>
2018-05-19 15:40:49 +0200

    Naive preprocessor.

--- semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/preprocessing/IPreprocessor.java
+++ semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/preprocessing/IPreprocessor.java
@@ -5,24 +5,26 @@ import java.util.List;
 /**
  * Rozhraní pro službu komplexního předzpracování slov na tokeny.
  *
- * @Author Radek Vais
+ * @author Radek Vais
  */
 public interface IPreprocessor {
 
     /**
      * Přípravná metoda očekává instnce stemmeru a tokenizátoru, pro správnou funkčnost preprocessingu.
-     * V případě, že instnce nebudou null, nebudou aplikována pravidla stemmingu nebo tokenizace.
+     * V případě, že instnce budou null, nebudou aplikována pravidla stemmingu nebo tokenizace.
+     *
+     * Různé implementace se mohou lišit v povinnch komponentách.
      *
      * @param stemmer - Instence stemmeru, která má být použita pro stemmování. Null pokud nechceme stemmovat.
      * @param tokenizer - Instance tokenizátoru, která má být použita pro tokenizci. Null pokud nechceme tokenizovat.
      */
-    void initilise(IStemmer stemmer, ITokenizer tokenizer);
+    void initialise(IStemmer stemmer, ITokenizer tokenizer);
 
     /**
      * Metoda předzpracuje předaný text a vrátí seznam, nalezených a vytvořených tokenů.
      * @param sentence - Libovolý řetězec dokumentu, ze kterého mají být vytvořeny tokeny.
      * @return Seznam extrahovaných tokenů.
-     * @throws IllegalStateException V případě, že nebyla volána metoda initialise() {@see initilise()}.
+     * @throws IllegalStateException V případě, že nejsou nastaveny povinné komponenty (volání initialise()) {@see initialise()}.
      */
     List<String> getProcessedForm(String sentence) throws IllegalStateException;
 
--- semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/preprocessing/Preprocessor.java
+++ semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/preprocessing/Preprocessor.java
@@ -3,23 +3,58 @@ package cz.zcu.kiv.nlp.ir.trec.preprocessing;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Instance preprocessingu, která vyžaduje ke korektnímu fungování tokenizér.
+ * Preprocesing provede nejdříve tokenizaci a na tokeny aplikuje stemming.
+ *
+ * @author Radek Vais
+ */
 public class Preprocessor implements IPreprocessor {
 
     /** instance loggeru */
-    static Logger logger = LogManager.getLogger(Preprocessor.class.getName());
+    private static Logger logger = LogManager.getLogger(Preprocessor.class.getName());
 
     private IStemmer stemmer;
     private ITokenizer tokenizer;
 
     @Override
-    public void initilise(IStemmer stemmer, ITokenizer tokenizer) {
+    public void initialise(IStemmer stemmer, ITokenizer tokenizer) {
+        logger.trace("Entry method");
+        if(stemmer != null){
+            logger.debug("Stemmer set up: "+stemmer);
+            this.stemmer = stemmer;
+        }
 
+        if(tokenizer != null){
+            logger.debug("Set up tokenizer: "+tokenizer);
+            this.tokenizer = tokenizer;
+        }
+        logger.trace("End method");
     }
 
     @Override
     public List<String> getProcessedForm(String sentence) {
-        return null;
+        logger.trace("Entry method");
+        if(tokenizer == null){
+            logger.warn("Tokenizer was not set");
+            throw new IllegalStateException("Tokenizer was not set.");
+        }
+
+        List<String> tokens = tokenizer.getTokens(sentence);
+
+        if(stemmer != null){
+            logger.debug("Steming");
+            List<String> stemmed = new ArrayList<>(tokens.size());
+            for (String token: tokens) {
+                stemmed.add(stemmer.stem(token));
+            }
+            tokens = stemmed;
+        }
+        logger.trace("End method");
+        return tokens;
     }
 }
