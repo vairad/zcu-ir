63b6432ec372b77cb7e31239a54e4512b344e23b
Radek Vais <vais.radek@gmail.com>
2018-05-22 13:58:48 +0200

    Update Mock object.

--- semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/gui/MockResutl.java
+++ semestralka/src/main/java/cz/zcu/kiv/nlp/ir/trec/gui/MockResutl.java
@@ -8,16 +8,16 @@ public class MockResutl implements IGuiResult {
 
     @Override
     public String getPresentQuery() {
-        return "Ano, ne, možná";
+        return "Ocekáváno";
     }
 
     @Override
     public String getMissingQuery() {
-        return "TOhle cybí";
+        return "Chybí";
     }
 
     @Override
     public String getPrewiev() {
-        return "Asi nějaký cool dokument";
+        return "Obsah";
     }
 }
